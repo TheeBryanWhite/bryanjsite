e0297d045b4c1f870fb1f89bbaefaafc36b8ccc9
Bryan White <bwhite@eastcoastcatalyst.com>
2021-01-19 15:53:56 -0500

    Still, with the favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -80,7 +80,7 @@ module.exports = {
         background_color: '#000',
         theme_color: '#fff',
         display: 'minimal-ui',
-        icon: 'src/images/favicon.png',
+        icon: 'src/images/gatsby-icon.png',
       },
     },
     {
