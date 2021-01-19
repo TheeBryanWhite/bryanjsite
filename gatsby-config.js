e20f4572c0eca55dcc782a695d58e43e2aab33a4
Bryan White <bwhite@eastcoastcatalyst.com>
2021-01-19 15:43:17 -0500

    Maybe changing the file name will work???

--- gatsby-config.js
+++ gatsby-config.js
@@ -80,7 +80,7 @@ module.exports = {
         background_color: '#000',
         theme_color: '#fff',
         display: 'minimal-ui',
-        icon: 'src/images/triangle.png',
+        icon: 'src/images/favicon.png',
       },
     },
     {
