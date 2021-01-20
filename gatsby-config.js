8daed0e30e44c3d80448badbda2cd856a729b9f5
Bryan White <bwhite@eastcoastcatalyst.com>
2021-01-19 22:18:15 -0500

    More post-launch plugin adds

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,39 +39,39 @@ module.exports = {
         },
       },
     },
-    // {
-    //   resolve: "gatsby-plugin-guess-js",
-    //   options: {
-    //     GAViewID: `230022083`,
-    //     jwt: {
-    //       client_email: "safercontact@safercontact-guess-js.iam.gserviceaccount.com",
-    //       private_key: "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQClBjuTIbUXOeWt\nXEJTAfHY7APVzi5IIzMGKcJKfU3KB4+xjrdcBrV0zxAILOjZrCYKMa9yy9ZJqwyH\nho9uBqkO7Zr2OY+RgTlXNu0quCIeErB+8jbVHGwwox6oG4E5vo+Z3Lx2J3t3HO+T\nxFEFXjNDM+KTeajlzNKHFdpOMdCnVAM1cny8ndNPXud6chQ/AHxJqjYuu1aMPX0s\nFu+Ogxstzr58ErOH509mOIOKuoz9ta2HcyDh/o3ZQzDo2gCeJ0DEYiFlmSD7cYU0\nuV65NbJW31wlOijlHdX3Xf+ZOGmSP70kiuqZuUewu6xg8aiES3TqRFu4pzmeURWD\nyEcBXXhbAgMBAAECggEADB5IDUbomHSjXbasfCuXyfLGJh/TUDxqSJj6cCPn3965\nE5qqWYyrxV2LQAs90ilzf0SLLYyzoL2tC6q69gmkX1J3En72ZwLGGWLdya+ARq5F\n3bY47Pwx8BeaboSoJnuZUGNgR426SFK/2R10FOCh0ENYIrV7zKkQCksPHxzgK9lZ\nbD5b34alcWjJxYsqoMa4Rh/zlpyYTQ52AsW+1iWJFkOy7+eOlWoHrRMq0bNCmhJ4\nWOaTbrKDBDDC4jFN55brhoS12BJ+qTZoO0hjiBluOHXhBfxVd0VRD/2IXjEck86S\nagZMIwHnS3F8UGH7KdL8+tHUUaXgCkmKeEtA1B9MKQKBgQDf7YeQ8sVLOB0mAr0u\nbCslgtqY/9XwCEhVxFjqqdG4zDRT9dcLNcCV4iJpQAPJZzXiGjgIpKGadh6twujR\nTSCo8AIctWteXgXAtXAYHOX2s47L4Uoy2BVMcY0w3j3a9VfCPLG+i5OS/C6qBpvw\nj0ml60NRpDsCmyNCtH9Z3/A+5QKBgQC8qPcYoFEEWS73/qmO8KVYTmPGEd/feaaV\nT/IbHN3wA8qhe+BkyafPco0n5SxvBi5NBMtexUlekTAzSSAFo7rhtq4xmxtOOK3a\nYw+Btvlh8BmOidjGzdRblDXK7Py6oTT4ttna4wFLcBH+5vwMKZNeG6dY8M31fYv6\nhbNnJcgmPwKBgBeulD7FHg51cce6VG2bbPi7nQi/Tc3j4lqHuGug6BgmKIaUoliC\nJ8ryLnd5GK1tJ+qwoSpw1tJyGY6A5YR1JrYXI/ILUb6aLPuTdLZoH/32QWzYd7Nm\n3OHHxIndhycn+U8GgjnHoyTRXNIIfudzD/PnLKhuuPruk00Tr8A+ibbtAoGAV14w\nQDlOC6N5euN2hnmvOLvkvL9eW9GscY06Y4/0v8ghuLLzcflpO0qjUAxzjV7hZk2I\nGPVoSuEV30pICBSQ4SKGfPaFqK3a5T3m2n1v9AkK7NWtjDOZ2mdCmh0x1QP+0gdr\n0FA8M0FNNCyoIMHIO1a3IxsxZjSj9HqiYGxAymUCgYA9YvA4U4cG6P7h8vmhQC80\nIOJcJQUVC1EML63i+EyzKZ4on4iUD2WH7rHa03Z7h1QSF9mZNzRTLN+Nkkl85P1Y\nxX952i1LQANCn4iPBHrxseKh9j33YjbmWiXkVLEMLu5SL+Z6fFfPwQqXPGNJoXwh\nANK1gCCwt8kfW/bv80vxVA==\n-----END PRIVATE KEY-----\n",
-    //     },
-    //     minimumThreshold: 0.03,
-    //     period: {
-    //       startDate: new Date("2018-1-1"),
-    //       endDate: new Date(),
-    //     },
-    //   },
-    // },
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     trackingId: "UA-179234948-1",
-    //     head: false,
-    //     pageTransitionDelay: 0,
-    //     defer: true,
-    //   },
-    // },
-    // {
-    //   resolve: 'gatsby-plugin-google-tagmanager',
-    //   options: {
-    //     id: 'GTM-58X3Z98',
-    //     includeInDevelopment: false,
-    //     defaultDataLayer: { platform: "gatsby" },
-    //     routeChangeEventName: "gatsby-route-change",
-    //   },
-    // },
+    {
+      resolve: "gatsby-plugin-guess-js",
+      options: {
+        GAViewID: `236013635`,
+        jwt: {
+          client_email: "bryanwhite@bryanjsite.iam.gserviceaccount.com",
+          private_key: "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDaE9D0Mygz6MME\ngJa0GT/KAj92uSalNNwk4I1nNQrWFgLmWfs+SxW3uDfDN8aY6bHGTdgNQU0a8iaO\n1qEzV/qZlSQIk1tptO6jM/4+Sg2eUPqF1NXPr6iNrl//f+J9IU/OzAabRaHkWxwu\n3umMGS512rFvggNp1d913XjdIWOz5iNCJ3a6iaU4MIppNyMx1XHStC4rl2tm7pxk\nCIlpOVPeRDxCrzlwCxysTOfrEbt0egbPqE9gu85+bvvdKg5POn9foe5bQp83nJ8+\nkUTP0/BSLvHmEJLkXldbdMEganRJ2c+kVQmWJqBHNBL8WI4pjomM//nLBwZXCR5x\nt1swJU6vAgMBAAECggEACAShuaftat8uLOAlsXeaot6An2iWVTgf8DaAuq7Ev0iu\nAoD2He7VI8ChCgQuB/WZ6/zm1THuZlNJCfn9PBiKLROJqXZjw8K0cCXUS6LIZJ+Z\neubE60tTJ1vCdCjV8yqe4b2MU/gtK7HZqSWDIt9hCcO2+L0Z2c5GOgA9VlcscHL3\naKvJDuRTcsrniDtIU7vUITMlsxvkkiOkNDpP3AUQj6iSOHZG2cGpIl8/Y927bl2s\nGaGwJHH+oHrDAE0YjRF3hXmsP/tbnGarT5/VdB4qyed1QancmmU6mii6winv+w1T\ncSkRQyjSmkb0WT2bpmWuoPpWusnLE7dWssjaIAdVmQKBgQDxb7501XsuJHRelxCr\nMlzhvFZ/zcv1d2KaRMM31EHt9ZbwR80EPZao+/tKOF8zwqXqFsYIB39X7JTvC9ow\nDCgZ3k+zNHg6+4oiCLplfOJ/mabs0RVpzuyExmk5PwDQPMKwwC8HO1IGzn5A/dvl\nsNq8ExEzs8LsY7CMPTCpR0TxKQKBgQDnO1ycokpRzCTj+D1eiiV6oSiKq+/pbaZ/\nKIkh81og/mCdhED4pFgh7T+3TUh2vqGmtweL0ZTD+yMHLJwuzIvMDUFzVjE2HDqn\nvxZtA3rF3ZbV0js5oUysgC/ABcqQjpbh22l+DHBkYC+hU6HQFMnc1bL0pX210TXP\nT29rhpcEFwKBgQDSwpNl9j4Tp6sWL5VTfSFrxTdVVddoLpGazdAE2nu7OJpMosEr\npH/ygBV7Uv54dsDTOpIgzDq0DEhu7M/JBbOz8Udtzq3t/0s0Lp5Ync4zSXRYYqTo\nusPpnDKa0I7rOT9TgFENZRFT88pu/ayKM/qeF576nT/Zj6S8p2XlP9vFEQKBgQCn\naLrsTDMydXJf+mG0CKCJ4BPSEhloDxL0DQz7n4wpRwH8CLxhR9rm9TWkKDfGgM3Y\nPbsg52OlYDVEBHAvEbhEy3QPZN/MJTPyLs61rkc86elCdSqb1sQEP+pSQ98cOZ/T\ndV8y+x6EDX9udXY5iFN9ptohdY9GC2nB5ejtGExNkQKBgHlA8Frn8Url5ACZBnmS\nrra8NPwhS7os6JJoP0NOeWx/QlAS8L9mvdxFlsnt3wo6ukgC7AgzXsO39/CX0hZW\nTWyOnOPydEoD2DmK3WDDnY5V6krSILB1t/qCaNLZE8L405sU2TxZKSAxerFuYmIn\nTdO6kmXsmuQDyFVdB3YwVlrj\n-----END PRIVATE KEY-----\n",
+        },
+        minimumThreshold: 0.03,
+        period: {
+          startDate: new Date("2018-1-1"),
+          endDate: new Date(),
+        },
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-167309418-2",
+        head: false,
+        pageTransitionDelay: 0,
+        defer: true,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-google-tagmanager',
+      options: {
+        id: 'GTM-MWZFZ66',
+        includeInDevelopment: false,
+        defaultDataLayer: { platform: "gatsby" },
+        routeChangeEventName: "gatsby-route-change",
+      },
+    },
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
@@ -110,18 +110,18 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    // {
-    //   resolve: 'gatsby-plugin-robots-txt',
-    //   options: {
-    //     host: '',
-    //     policy: [{
-    //       userAgent: '*',
-    //       allow: '/',
-    //     }],
-    //     output: '/robots.txt',
-    //     sitemap: 'https://www.bryanjwhite.com/sitemap.xml'
-    //   },
-    // },   
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        host: '',
+        policy: [{
+          userAgent: '*',
+          allow: '/',
+        }],
+        output: '/robots.txt',
+        sitemap: 'https://www.bryanjwhite.com/sitemap.xml'
+      },
+    },   
     `gatsby-plugin-svgr`,
     `gatsby-plugin-sharp`,
     {
